e4d59586ff0498d60187b876a125187b2b9dcf57
KyofuNoKamen <oleksiykaras2016@gmail.com>
2023-02-22 19:44:58 +0200

    Added some files

---
+++ store/tests/test_api.py
@@ -0,0 +1,41 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from store.models import Book
+from store.serializers import BookSerializer
+
+
+class BooksApiTestCase(APITestCase):
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name="Author 1")
+        self.book_2 = Book.objects.create(name='Test book 2', price=75, author_name="Author 5")
+        self.book_3 = Book.objects.create(name='Author 1 Test book 3', price=50, author_name="Author 2")
+
+    def test_get(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_data = BookSerializer([self.book_1, self.book_2, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BookSerializer([self.book_1, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_sorting(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': 'price'})
+        serializer_data = BookSerializer([self.book_1, self.book_3, self.book_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_sorting2(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': 'name'})
+        serializer_data = BookSerializer([self.book_3, self.book_1, self.book_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
